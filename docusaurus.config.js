588b6fb0a7d718fc8125c3bcafd0055c9ad946bd
Fadi <fdkhadra@gmail.com>
2020-06-03 16:51:43 +0200

    add banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,8 +26,8 @@ module.exports = {
     },
     announcementBar: {
       id: "sponsor", // Any value that will identify this message
-      content:
-        'Hey you like my work? Consider <a target="_blank" rel="noopener noreferrer" href="https://github.com/sponsors/fkhadra">sponsoring</a> me',
+      content: 'Black lives matter.<a href="https://support.eji.org/give/153413/#!/donation/checkout" target="_blank">Support the Equal Justice Initiative.</a>'
+        // 'Hey you like my work? Consider <a target="_blank" rel="noopener noreferrer" href="https://github.com/sponsors/fkhadra">sponsoring</a> me',
     },
     algolia: {
       apiKey: "f54fc6c27b73c67cf37ad6f02753423b",
