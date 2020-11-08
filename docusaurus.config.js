72bad439be2ec4b5392808a61032fba676e7eda5
Fadi <fdkhadra@gmail.com>
2020-11-08 19:51:55 +0100

    put back sponsor

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,7 @@ module.exports = {
     },
     announcementBar: {
       id: "sponsor", // Any value that will identify this message
-      content: 'Black lives matter.<a href="https://support.eji.org/give/153413/#!/donation/checkout" target="_blank">Support the Equal Justice Initiative.</a>'
-        // 'Hey you like my work? Consider <a target="_blank" rel="noopener noreferrer" href="https://github.com/sponsors/fkhadra">sponsoring</a> me',
+      content: 'Hey you like my work? Consider <a target="_blank" rel="noopener noreferrer" href="https://github.com/sponsors/fkhadra">sponsoring</a> me',
     },
     algolia: {
       apiKey: "f54fc6c27b73c67cf37ad6f02753423b",
